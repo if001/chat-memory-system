32e3e1631cee3753dc91d63fa3b33d70dddb9470
windows_on_wsl <otomijuf.004@gmail.com>
2026-08-31 23:28:00 +0900

    feat: expose canonical turn record reader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./memory_system/domain/types";
 export * from "./memory_system/api/service";
 export * from "./memory_system/api/backgroundRunner";
+export * from "./memory_system/api/turnRecordReader";
 export * from "./memory_system/infrastructure/ollama/fileCachedClient";
